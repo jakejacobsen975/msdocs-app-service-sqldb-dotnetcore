7c1501cc62b228620eb05d3b40ca356f112c725c
jakejacobsen975 <122470500+jakejacobsen975@users.noreply.github.com>
2025-03-13 21:05:34 +0000

    Log all configuration keys and their values during application startup

--- Program.cs
+++ Program.cs
@@ -14,8 +14,10 @@ if (builder.Environment.IsDevelopment())
 }
 else
 {
-    var azureSqlConnection = builder.Configuration["AZURE_SQL_CONNECTIONSTRING"];
-    Console.WriteLine($"Retrieved AZURE_SQL_CONNECTIONSTRING: {azureSqlConnection}");
+    foreach (var key in builder.Configuration.AsEnumerable())
+{
+    Console.WriteLine($"Key: {key.Key}, Value: {key.Value}");
+}
     builder.Services.AddDbContext<MyDatabaseContext>(options =>
         options.UseSqlServer(builder.Configuration.GetConnectionString("AZURE_SQL_CONNECTIONSTRING")));
     builder.Services.AddStackExchangeRedisCache(options =>
