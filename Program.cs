38dc88d5ee786aa12655e9490ee0d498c07fe4e2
jakejacobsen975 <122470500+jakejacobsen975@users.noreply.github.com>
2025-03-13 20:54:14 +0000

    Enhance database context configuration for development and production environments

--- Program.cs
+++ Program.cs
@@ -6,12 +6,16 @@ var builder = WebApplication.CreateBuilder(args);
 // Add database context and cache
 if (builder.Environment.IsDevelopment())
 {
+    
     builder.Services.AddDbContext<MyDatabaseContext>(options =>
         options.UseSqlServer(builder.Configuration.GetConnectionString("MyDbConnection")));
     builder.Services.AddDistributedMemoryCache();
+    
 }
 else
 {
+    var azureSqlConnection = builder.Configuration.GetConnectionString("AZURE_SQL_CONNECTIONSTRING");
+    Console.WriteLine($"Retrieved AZURE_SQL_CONNECTIONSTRING: {azureSqlConnection}");
     builder.Services.AddDbContext<MyDatabaseContext>(options =>
         options.UseSqlServer(builder.Configuration.GetConnectionString("AZURE_SQL_CONNECTIONSTRING")));
     builder.Services.AddStackExchangeRedisCache(options =>
