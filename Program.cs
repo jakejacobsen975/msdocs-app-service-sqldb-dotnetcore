7b9b95f187453517826da8ab0e71251fde618122
jakejacobsen975 <122470500+jakejacobsen975@users.noreply.github.com>
2025-03-13 21:14:47 +0000

    fixed connection

--- Program.cs
+++ Program.cs
@@ -14,8 +14,7 @@ if (builder.Environment.IsDevelopment())
 }
 else
 {
-    var azureSqlConnection = builder.Configuration["AZURE_SQL_CONNECTIONSTRING"];
-    Console.WriteLine($"Retrieved AZURE_SQL_CONNECTIONSTRING: {azureSqlConnection}");
+    
     builder.Services.AddDbContext<MyDatabaseContext>(options =>
         options.UseSqlServer(builder.Configuration["AZURE_SQL_CONNECTIONSTRING"]));
     builder.Services.AddStackExchangeRedisCache(options =>
